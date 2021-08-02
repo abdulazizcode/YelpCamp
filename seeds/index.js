08ac87bbc0d543b6a205aecfb984b76c0daa8fe7
abdulazizcode <tajikcoder@gmail.com>
2021-08-02 09:57:59 +0800

    i pkg bootstrap cdn & styles

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description:
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos porro eum praesentium dolor sapiente voluptas delectus illum odit! Doloremque architecto reprehenderit quis culpa voluptatibus odio nisi commodi sit vero similique.",
+            price,
         });
         await camp.save();
     }
